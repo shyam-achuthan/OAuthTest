6eb1e643e424728d3b8fc6086eee2b9a45ef51c9
adarshk <appusk2004@gmail.com>
2014-11-18 13:32:04 +0000

    commit ad 1

--- api/index.php
+++ api/index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'testing');
+	define('ENVIRONMENT', 'testing1');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
